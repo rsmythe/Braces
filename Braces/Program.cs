a2a8719249c56ab5ad2acdc0b91c220f7b6457fb
Ryan Smythe <ryan.smythe@gmail.com>
2017-11-08 23:14:44 -0500

    initial checkin

---
+++ Braces/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Braces
+{
+    class Program
+    {
+        static string[] test = new string[] { "{[}]}", "{}[{}]([])", "{{[]", "[{((()))}]", "{({()}}"};
+        static string[] testAssert = new string[] { "NO", "YES", "NO", "YES", "NO" };
+
+        static void Main(string[] args)
+        {
+            string[] results = braces(test);
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                Console.WriteLine($"Test {i + 1}: " + ((results[i] == testAssert[i]) ? "SUCCEEDED" : "FAILED"));
+            }
+            Console.ReadKey();
+        }
+
+        static string[] braces(string[] values)
+        {
+            Dictionary<char, char> bracePairs = new Dictionary<char, char>();
+            bracePairs['}'] = '{';
+            bracePairs[']'] = '[';
+            bracePairs[')'] = '(';
+
+            List<string> results = new List<string>();
+
+            foreach (string str in values)
+            {
+                // If openBraces have not all been closed, return NO
+                results.Add(AreBracesBalanced(bracePairs, str) ? "YES" : "NO");
+            }
+
+            return results.ToArray();
+        }
+
+        private static bool AreBracesBalanced(Dictionary<char, char> bracePairs, string str)
+        {
+            Stack<char> openBraces = new Stack<char>();
+
+            foreach (char c in str)
+            {
+                // If this is a closing brace
+                if (bracePairs.ContainsKey(c))
+                {
+                    // Remove the current top level brace if it's a match
+                    if (bracePairs[c] == openBraces.Peek())
+                    {
+                        openBraces.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    openBraces.Push(c);
+                }
+            }
+
+            return !openBraces.Any();
+        }
+    }
+}
